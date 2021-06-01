b7863a774485a0a4f4a15026640e0f489ca92ab7
ylan sayada <sayylan@gmail.com>
2021-06-01 09:55:09 +0300

    add style to article.

--- src/common/mainComponents/Article.tsx
+++ src/common/mainComponents/Article.tsx
@@ -1,8 +1,7 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import RoundedIcone from '../components/ect/RoundedIcone';
 import me from '../../ressources/img/me.jpeg';
-import Img from '../components/ect/Img';
 import FineSep from '../components/sep/FineSep';
 import SocialPanel from './SocialPanel';
 
@@ -29,30 +28,39 @@ let article = {
 //     column?: boolean
 // }
 export default function Article() {
+    useEffect(() => {
+        document.body.style.backgroundColor = "#eeeeee";
+        return () => {
+            document.body.style.backgroundColor = "initial"
+            // Anything in here is fired on component unmount.
+        }
+    }, [])
     let { id }: { id: string } = useParams();
-
     return (
         <div className="article" dir="rtl">
-            <div className="head-article">
-                <h1>{article.title}</h1>
-                <div className="publication-desc">
-                    <RoundedIcone image={me} />
-                    <div className="text-desc">
-                        <span className="author-name">{article.author}</span>
-                        <span className="sub-desc" >פורסם ב-{article.publishDate.toLocaleDateString()}</span>
+            <div className="head-article" style={{ backgroundImage: `url(${article.img})` }}>
+                <div className="shadow-border">
+                    <h1>{article.title}</h1>
+                    <div className="publication-desc">
+                        <RoundedIcone image={me} />
+                        <div className="text-desc">
+                            <span className="author-name">{article.author}</span>
+                            <span className="sub-desc" >פורסם ב-{article.publishDate.toLocaleDateString()}</span>
+                        </div>
                     </div>
                 </div>
-                <div className="content">
-                    <Img src={article.img} />
-                    <h4>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Repellendus mollitia dicta ipsum dolori</h4>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quasi laborum mollitia hic fugiat aspernatur excepturi minima magni quidem incidunt reiciendis. Vitae sit deserunt aut temporibus, excepturi sint adipisci dolor deleniti!
-                    Praesentium tempora libero natus magni modi fuga hic dolores adipisci, architecto assumenda, fugit minus eum error similique beatae accusamus nesciunt nihil tempore ipsam. Sunt unde ratione at, ut ab nesciunt?
-                    Laborum dolores porro enim quia nemo aliquid labore. Cumque facere dolorum minus repellendus distinctio consectetur. Esse explicabo quam recusandae autem eveniet sunt excepturi! Molestias, unde. Accusamus modi voluptatem a vel.
-                    Deserunt provident fuga ex quis itaque a dicta ipsa eveniet cum similique eaque, possimus corporis nesciunt dignissimos officia! Consequuntur officia ab consectetur voluptate distinctio nulla veritatis veniam fuga sequi exercitationem.
+
+            </div>
+            <div className="content">
+
+                <h3>Lorem ipsum dolor, sit amet consectetur adipisicing elit.</h3>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quasi laborum mollitia hic fugiat aspernatur excepturi minima magni quidem incidunt reiciendis. Vitae sit deserunt aut temporibus, excepturi sint adipisci dolor deleniti!
+                Praesentium tempora libero natus magni modi fuga hic dolores adipisci, architecto assumenda, fugit minus eum error similique beatae accusamus nesciunt nihil tempore ipsam. Sunt unde ratione at, ut ab nesciunt?
+                Laborum dolores porro enim quia nemo aliquid labore. Cumque facere dolorum minus repellendus distinctio consectetur. Esse explicabo quam recusandae autem eveniet sunt excepturi! Molestias, unde. Accusamus modi voluptatem a vel.
+                Deserunt provident fuga ex quis itaque a dicta ipsa eveniet cum similique eaque, possimus corporis nesciunt dignissimos officia! Consequuntur officia ab consectetur voluptate distinctio nulla veritatis veniam fuga sequi exercitationem.
                 Unde deleniti in dolorem amet excepturi officiis molestiae minus asperiores sed autem facilis, accusamus necessitatibus quas minima possimus repellendus numquam nam dolor voluptate cum esse. Ratione expedita eligendi autem aut!</p>
-                    <h4>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Repellendus mollitia dicta ipsum dolori</h4>
-                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sit minus repudiandae culpa nostrum temporibus nobis laboriosam animi nesciunt, fugit quaerat ducimus praesentium dolor consequuntur, tempora quae earum voluptate, error similique?</p>
-                </div>
+                <h3>Lorem ipsum dolor, sit amet consectetur adipisicing elit. </h3>
+                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sit minus repudiandae culpa nostrum temporibus nobis laboriosam animi nesciunt, fugit quaerat ducimus praesentium dolor consequuntur, tempora quae earum voluptate, error similique?</p>
                 <FineSep />
                 <SocialPanel id={id} />
             </div>
--- src/common/mainComponents/Footer.tsx
+++ src/common/mainComponents/Footer.tsx
@@ -1,26 +1,27 @@
 import React from 'react'
-import {ReactComponent as SvgFacebook} from '../../ressources/img/icone/facebookBlack.svg';
-import {ReactComponent as SvgLinkedin} from '../../ressources/img/icone/linkedin.svg';
-import MailOutlineTwoToneIcon  from '@material-ui/icons/MailOutlineTwoTone';
-import {ReactComponent as SvgWave} from '../../ressources/img/wave.svg';
+import { ReactComponent as SvgFacebook } from '../../ressources/img/icone/facebookBlack.svg';
+import { ReactComponent as SvgLinkedin } from '../../ressources/img/icone/linkedin.svg';
+import MailOutlineTwoToneIcon from '@material-ui/icons/MailOutlineTwoTone';
+import { ReactComponent as SvgWave } from '../../ressources/img/wave.svg';
 export default function Footer() {
+
     return (
         <div className="footer">
             <div className="footer-content">
-                <SvgWave/>
+                <SvgWave />
                 <div className="socialIcones" >
-            <a className="facebook-icone" href="https://www.facebook.com/ylan.sayada/" target="_blank" rel="noreferrer">
-                <SvgFacebook fill="currentColor" />
-            </a>
-            <a className="linkedin-icone" href="https://www.linkedin.com/in/ylan-sayada-1363311b8/" target="_blank" rel="noreferrer">
-                <SvgLinkedin fill="currentColor" />
-            </a>
-            <a className="mail-icone" href="mailto:sayylan@gmail.com" target="_blank" rel="noreferrer">
-               <MailOutlineTwoToneIcon/>
-            </a>
+                    <a className="facebook-icone" href="https://www.facebook.com/ylan.sayada/" target="_blank" rel="noreferrer">
+                        <SvgFacebook fill="currentColor" />
+                    </a>
+                    <a className="linkedin-icone" href="https://www.linkedin.com/in/ylan-sayada-1363311b8/" target="_blank" rel="noreferrer">
+                        <SvgLinkedin fill="currentColor" />
+                    </a>
+                    <a className="mail-icone" href="mailto:sayylan@gmail.com" target="_blank" rel="noreferrer">
+                        <MailOutlineTwoToneIcon />
+                    </a>
+                </div>
             </div>
-            </div>
-            
+
         </div>
     )
 }
